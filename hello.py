1bc3fc2ffbb8c467c2a70c0df78cab675b7097d7
Jasper Gregory <jaspergregory@gmail.com>
2015-01-02 18:02:37 -0800

    in progress

--- hello.py
+++ hello.py
@@ -8,13 +8,6 @@ from flask import request
 from flask import Flask
 
 app = Flask(__name__, static_url_path='')
-# app.config.from_object('config')
-
-
-
-# @app.route('/')
-# def root():
-#     return app.send_static_file('index.html')
 
 
 @app.route('/ngram', methods = ['GET','POST'])
@@ -25,20 +18,23 @@ def root():
 
     if (request.method=='GET') :
         query = request.args.get('query', default='feminism')
-        corpus = request.args.get('corpus', default='eng_us_2012')
+        corpus = request.args.get('corpus', default='eng_2012')
+        startDate = request.args.get('startdate', type=int, default=1880)
+        endDate = request.args.get('enddate', type=int, default=2008)
     #query = 'feminism'
     # corpus = 'eng_us_2012'
     # request.data()
     if (request.method=='POST') :
         if (request.json) :
-            corpus = request.json.get('corpus','eng_us_2012')
+            corpus = request.json.get('corpus','eng_2012')
             query = request.json.get('query','feminism')
+            startDate = request.json.get('startdate', 1880)
+            endDate = request.json.get('enddate', 2008)
         else :
-            corpus = request.form.get('corpus','eng_us_2012')
+            corpus = request.form.get('corpus','eng_2012')
             query = request.form.get('query','feminism')
-
-    startDate = 1880
-    endDate = 2008
+            startDate = request.form.get('startdate', 1880)
+            endDate = request.form.get('enddate', 2008)
 
     df = getNgrams(query, corpus, startDate, endDate, 0, False)[2]
 
@@ -47,7 +43,7 @@ def root():
                          x_axis_format="", y_axis_format="2f", jquery_on_ready=False)
 
 
-    chart.set_containerheader("\n\n<h1>The term '" + type + "' as a percentage of all published American English words (1880-2008)</h1>\n\n")
+    chart.set_containerheader("\n\n<h1>The term '" + type + "' as a portion of all published English words (1880-2008)</h1><h4>" + df.max()[0].astype(int).astype('str') + " set to 100.</h4>\n\n")
 
     extra_serie = {"tooltip": {"y_start": "", "y_end": " "}}
 
@@ -57,7 +53,7 @@ def root():
     for value in df.columns.values:
         d=df[value]
         if (value != "year"):
-            chart.add_serie(name=value, y=(d.values/maxnum*100).astype("int"), x=d.keys(), extra=extra_serie )
+            chart.add_serie(name=value, y=(d.values/maxnum*100).astype("int"), x=d.keys().astype(int), extra=extra_serie )
         else:
             print "skipping year"
 
@@ -96,6 +92,19 @@ def getNgrams(query, corpus, startYear, endYear, smoothing, caseInsensitive):
     return req.url, params['content'], df
 
 if __name__ == '__main__':
-    app.run()
+    import argparse
+    parser = argparse.ArgumentParser(description='Development Server Help')
+    parser.add_argument("-d", "--debug", action="store_true", dest="debug_mode",
+                  help="run in debug mode (for use with PyCharm)", default=False)
+    parser.add_argument("-p", "--port", dest="port",
+                  help="port of server (default:%(default)s)", type=int, default=5000)
 
+    cmd_args = parser.parse_args()
+    app_options = {"port": cmd_args.port }
 
+    if cmd_args.debug_mode:
+        app_options["debug"] = True
+        app_options["use_debugger"] = False
+        app_options["use_reloader"] = False
+
+    app.run(**app_options)
